5858c852fc0254c853fb037d1096779eb0962246
liumijian <liumijian@outlook.com>
2020-12-16 18:56:32 +0800

    master

--- src/main.cpp
+++ src/main.cpp
@@ -51,16 +51,58 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
+
+    float start_x, start_y, end_x, end_y;
+    float s_x, s_y, e_x, e_y;
+    std::cout << "Input values from 0 to 100" << "\n";
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+    std::cout << "x coordinate of starting point: ";
+    std::cin >> s_x;
+    if (s_x <= 100 && s_x >= 0) {
+        start_x = s_x;
+    }
+    else {
+        std::cout << "Error: value is outside the limits" << "\n";
+    }
+
+    std::cout << "y coordinate of starting point: ";
+    std::cin >> s_y;
+    if (s_y <= 100 && s_y >= 0) {
+        start_y = s_y;
+    }
+    else {
+        std::cout << "Error: value is outside the limits" << "\n";
+    }
+
+    std::cout << "x coordinate of goal: ";
+    std::cin >> e_x;
+    if (e_x <= 100 && e_x >= 0) {
+        end_x = e_x;
+    }
+    else {
+        std::cout << "Error: value is outside the limits" << "\n";
+    }
+
+    std::cout << "y coordinate of goal: ";
+    std::cin >> e_y;
+    if (e_y <= 100 && e_y >= 0) {
+        end_y = e_y;
+    }
+    else {
+        std::cout << "Error: value is outside the limits" << "\n";
+    }
+
+    std::cout << "You have entered start point as: " << start_x << "," << start_y << "\n";
+    std::cout << "You have entered goal as: " << end_x << "," << end_y << "\n";
+
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
+
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
